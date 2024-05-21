682858b05a99860769af8881c34d46ee92ede5e4
Thomas <117021963+thomaswillix@users.noreply.github.com>
2024-05-21 21:50:08 +0200

    .
    .

--- Colegio/src/main/Main.java
+++ Colegio/src/main/Main.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -15,19 +18,35 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         Connection con = DriverManager.getConnection("“jdbc:mysql://localhost/colegio", "root", "");
-        PreparedStatement pst = con.prepareStatement("update alumnos set Nota = ? where id = ?");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from alumnos");
         
-        Statement st  = con.createStatement();
-        ResultSet rs = st.executeQuery("select *  from cambios");
+        HashMap<Integer,Integer> notas = new HashMap<Integer,Integer>();
+        while(rs.next()){
+            notas.put(rs.getInt(1), rs.getInt(4));
+        }
+        PreparedStatement update = con.prepareStatement("update alumnos set Nota = ? where id = ?");
+        PreparedStatement delete = con.prepareStatement("delete from Alunos where id=?");
+
+        rs = st.executeQuery("select * from cambios");
+
         String operacion;
         int id, incremento;
-        
-        while (rs.next()) {            
+
+        while (rs.next()) {
             operacion = rs.getString(2);
             id = rs.getInt(3);
             incremento = rs.getInt(4);
             switch(operacion){
-                
+                case "BJ": 
+                    delete.setInt(1, id);
+                    delete.execute();
+                    break;
+                case "MD":
+                    update.setInt(1, notas.get(id) + incremento);
+                    update.setInt(2, id);
+                    update.execute();
+                    break;
             }
         }
     }
