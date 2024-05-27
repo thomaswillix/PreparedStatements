0728e9adb37673b9fb1e614ee49d06cbff8e2d7a
Thomas <117021963+thomaswillix@users.noreply.github.com>
2024-05-27 17:32:45 +0200

    .
    Corrección de algunas cosas

--- Libros/src/util/Main.java
+++ Libros/src/util/Main.java
@@ -4,7 +4,6 @@ import classes.Libro;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -36,32 +35,41 @@ public class Main {
         } while (precio<=0);
         
         Libro.setPrecio(con, precio);
-        
         Libro.verCatalogo(con);
         
-        System.out.println("--------- Set precio máximo entre dos libros ---------");
+        System.out.println("\n--------- Set precio máximo entre dos libros ---------");
+        System.out.print("Primer ");
         isbn = comprobacionISBN(isbn, sc);
+        System.out.print("Segundo ");
         isbn2 = comprobacionISBN(isbn2, sc);
         precio = comprobacionPrecio(precio, sc);
         
         Libro.setPrecioPorIsbn(con, comprobacion, isbn, isbn2, precio);
         Libro.verCatalogo(con);
         
-        System.out.println("--------- Actualizar paginas y precio por isbn ---------");
+        System.out.println("\n--------- Actualizar paginas y precio por isbn ---------");
         isbn = comprobacionISBN(isbn, sc);
         System.out.println("El número de páginas que pongas a continuación se añadirá a las que ya había previamente");
         paginas = comprobacionPaginas(paginas, sc);
         precioF = (float)comprobacionPrecio(precio, sc);
         Libro.actualizarPaginasYPrecio(con, comprobacion, isbn, paginas, precioF);
         Libro.verCatalogo(con);
-        System.out.println("------------------ Clonar un libro ------------------");
+        System.out.println("\n------------------ Clonar un libro ------------------");
+        System.out.print("El primer ISBN será el libro que copies y el segundo la copia (tendrás que poner un nuevo ISBN)\n\nPrimer ");
         isbn = comprobacionISBN(isbn, sc);
+        System.out.print("Segundo ");
         isbn2 = comprobacionISBN(isbn2, sc);
+        
         Libro.InsertarCopiaLibro(con, comprobacion, isbn, isbn2);
+        Libro.verCatalogo(con);
+        
+        st.close();
+        comprobacion.close();
+        con.close();
     }
     private static int comprobacionISBN(int isbn, Scanner sc){
         do {            
-            System.out.println("ISBN 1: ");
+            System.out.println("ISBN: ");
             isbn = sc.nextInt();
             if (isbn<=0) {
                 System.err.println("El isbn no puede ser menor o igual a 0");
